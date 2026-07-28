aee636c709c17c5666129192fa46d0b4420bf2e8
Alishan Nadeem <alishan.nadeem22@gmail.com>
2026-07-28 11:31:20 +0500

    Update geolocator dependency to version 13.0.4 in pubspec.yaml and remove unused dependencies from pubspec.lock and GeneratedPluginRegistrant.swift. This streamlines the project by eliminating unnecessary packages and ensuring compatibility with the updated geolocation functionality.

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -10,7 +10,6 @@ import file_picker
 import file_selector_macos
 import flutter_secure_storage_darwin
 import geolocator_apple
-import package_info_plus
 import path_provider_foundation
 import printing
 import share_plus
@@ -24,7 +23,6 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FlutterSecureStorageDarwinPlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStorageDarwinPlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
-  FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   PrintingPlugin.register(with: registry.registrar(forPlugin: "PrintingPlugin"))
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
